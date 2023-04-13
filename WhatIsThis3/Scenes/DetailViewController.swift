504854ffbbfec2146011237ad3bb884b205437e3
alsldmswl <jej1452@gmail.com>
2023-04-13 23:31:07 +0900

    Firebase storage에 이미지 업로드 코드 추가

--- WhatIsThis3/Scenes/DetailViewController.swift
+++ WhatIsThis3/Scenes/DetailViewController.swift
@@ -8,34 +8,30 @@
 //TODO: 이미지 저장기능, 데이터 베이스 저장기능 수정
 
 import UIKit
-import CoreData
+import Firebase
+import FirebaseStorage
 
 //protocol DetailViewControllerDelegate: AnyObject {
 //    func fetchImage()
 //}
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailCollectionView: UICollectionView!
-
+    
+    
     @IBOutlet weak var detailTableView: UITableView!
     @IBOutlet weak var commentTextField: UITextField!
     
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var DetailImageView: UIImageView!
     
-    let appdelegate = UIApplication.shared.delegate as! AppDelegate
     
-//    weak var delegate: DetailViewControllerDelegate?
+    //    weak var delegate: DetailViewControllerDelegate?
     var selectedCustomerList: Customer?
     var indexPath: IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        detailCollectionView.collectionViewLayout = layout()
-        detailCollectionView.delegate = self
-        detailCollectionView.dataSource = self
         
     }
     
@@ -45,34 +41,12 @@ class DetailViewController: UIViewController {
         if let hasData = selectedCustomerList {
             titleLabel.text = hasData.title
             contentLabel.text = hasData.content
+            guard let urlString = UserDefaults.standard.string(forKey: "myImageUrl") else {return}
+            FirebaseStorageManager.downloadImage(urlString: urlString) { [weak self] image in
+                self?.DetailImageView.image = image
+            }
         }
-    }
- 
-    private func layout() -> UICollectionViewCompositionalLayout {
-        
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
+      
     }
     
-    
-}
-
-extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailVCCell", for: indexPath) as! DetailCollectionViewCell
-       
-        return cell
-    }
-
 }
--- WhatIsThis3/Scenes/WriteViewController.swift
+++ WhatIsThis3/Scenes/WriteViewController.swift
@@ -21,21 +21,19 @@ class WriteViewController: UIViewController {
     let db = Database.database().reference()
     var customers: [Customer] = []
     
-    var fetchResults: PHFetchResult<PHAsset>?
-
+    //    var fetchResults: PHFetchResult<PHAsset>?
+    
     weak var delegate1: WriteViewControllerDelegate1?
-  
-    @IBOutlet weak var writeCollectionView: UICollectionView!
+    
+//    @IBOutlet weak var writeCollectionView: UICollectionView!
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        writeCollectionView.collectionViewLayout = layout()
-        writeCollectionView.delegate = self
-        writeCollectionView.dataSource = self
-    
+        
     }
     
     
@@ -55,96 +53,25 @@ class WriteViewController: UIViewController {
     }
     
     @IBAction func inputImageButton(_ sender: UIButton) {
-        checkPermission()
+        presentPhotoPicker()
     }
     
-    func checkPermission() {
-        if PHPhotoLibrary.authorizationStatus() == .authorized || PHPhotoLibrary.authorizationStatus() == .limited {
-            DispatchQueue.main.async {
-                self.showGallery()
-            }
-           
-        } else if PHPhotoLibrary.authorizationStatus() == .denied {
-            DispatchQueue.main.async {
-                self.showAuthorizationDeniedAlert()
-            }
-            
-        } else if PHPhotoLibrary.authorizationStatus() == .notDetermined {
-            PHPhotoLibrary.requestAuthorization { status in
-                self.checkPermission()
-            }
-        }
-    }
-    
-    func showAuthorizationDeniedAlert() {
-        let alert = UIAlertController(title: "포토라이브러리 접근 권한을 활성화 해주세요.", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "닫기", style: .cancel))
-        alert.addAction(UIAlertAction(title: "설정으로 가기", style: .default, handler: { action in
-            guard let url = URL(string: UIApplication.openSettingsURLString) else {return}
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-            
-        }))
-        
-        self.present(alert, animated: true)
-    }
-    
-    func showGallery() {
-        let library = PHPhotoLibrary.shared()
-        
-        var configuration = PHPickerConfiguration(photoLibrary: library)
-        configuration.selectionLimit = 10
-        
-        let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = self
-        present(picker, animated: true)
-    }
-    
-    
-    private func layout() -> UICollectionViewCompositionalLayout {
-        
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
     }
     
 }
 
-extension WriteViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.fetchResults?.count ?? 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WriteVCCell", for: indexPath) as! WriteVCCell
-        if let asset = self.fetchResults?[indexPath.row] {
-            cell.loadImage(asset: asset)
-        }
-        return cell
-    }
-    
-    
-}
 
-extension WriteViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        let identifiers = results.map { $0.assetIdentifier ?? "" }
-        self.fetchResults = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: nil)
-        self.writeCollectionView.reloadData()
-        self.dismiss(animated: true)
-    }
-  
-}
 extension WriteViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage,
               let user = Auth.auth().currentUser else {return}
+        self.imageView.image = selectedImage
         
         FirebaseStorageManager.uploadImage(image: selectedImage, pathRoot: user.uid) { url in
             if let url = url {
@@ -155,6 +82,7 @@ extension WriteViewController: UIImagePickerControllerDelegate, UINavigationCont
         
     }
 }
+
 struct Customer: Codable {
     let id: String
     let title: String
